6eff46266cde898a951d86d5d64036d995e1c026
lucky-boy <1107660581@qq.com>
2020-04-29 11:45:06 +0800

    Initial Commit

---
+++ AppWithTheme/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppWithTheme
+//
+//  Created by Ekko on 2020/4/29.
+//  Copyright © 2020 Ekko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppWithTheme/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppWithTheme
+//
+//  Created by Ekko on 2020/4/29.
+//  Copyright © 2020 Ekko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
